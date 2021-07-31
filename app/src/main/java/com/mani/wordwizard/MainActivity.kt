8a0ea4cd73173ea88ab75334c2b9e19f420aac16
Mani <manigarg0000@gmail.com>
2021-08-01 01:56:19 +0530

    added new word activity and layout

--- app/src/main/java/com/mani/wordwizard/MainActivity.kt
+++ app/src/main/java/com/mani/wordwizard/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.mani.wordwizard
 //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
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fab = findViewById<FloatingActionButton>(R.id.fab_addNew)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddWordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
