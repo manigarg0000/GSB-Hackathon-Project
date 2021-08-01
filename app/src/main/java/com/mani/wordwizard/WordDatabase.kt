5a301ac47d104b53b74bb383af50a4459294231e
Mani <manigarg0000@gmail.com>
2021-08-01 09:45:23 +0530

    added repository file

--- app/src/main/java/com/mani/wordwizard/WordDatabase.kt
+++ app/src/main/java/com/mani/wordwizard/WordDatabase.kt
@@ -1,6 +1,5 @@
 package com.mani.wordwizard
 
-import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,13 +7,13 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Word::class], version = 1, exportSchema = false )
 abstract class WordDatabase : RoomDatabase(){
-    
+
     abstract fun wordDao():Word
 
     companion object{
         @Volatile
         private var INSTANCE : WordDatabase? = null
-        fun getDatabase(context : Context) : WordDatabase {
+        fun getDatabase(context: WordRepository) : WordDatabase {
             return INSTANCE ?: synchronized(this){
                 var instance = Room.databaseBuilder(
                     context.applicationContext,
---
+++ app/src/main/java/com/mani/wordwizard/WordRepository.kt
@@ -0,0 +1,22 @@
+package com.mani.wordwizard
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository(private val wordDao: WordDao) {
+
+    val allWords : Flow<List<Word>> = wordDao.getWords()
+
+    @Suppress("RedundantSuppressModifier")
+    @WorkerThread
+    suspend fun insert(word: Word){
+        wordDao.insert(word)
+    }
+
+    @Suppress("RedundantSuppressModifier")
+    @WorkerThread
+    suspend fun delete(word: Word){
+        wordDao.delete(word)
+    }
+
+}
