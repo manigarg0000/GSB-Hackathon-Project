8ba42b9d4e84b2819b232cd1f20b7c5031b11db5
Mani <manigarg0000@gmail.com>
2021-08-01 09:36:18 +0530

    added entity and Dao

--- app/src/main/java/com/mani/wordwizard/WordDao.kt
+++ app/src/main/java/com/mani/wordwizard/WordDao.kt
@@ -1,4 +1,20 @@
 package com.mani.wordwizard
 
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface WordDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word:Word)
+
+    @Delete
+    suspend fun delete(word: Word)
+
+    @Query("SELECT * FROM WORD_TABLE ORDER BY word ASC")
+    fun getWords() : Flow<List<Word>>
+
 }
