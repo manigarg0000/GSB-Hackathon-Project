292f9872b884786c21ea5133ebd2dae931131c44
Mani <manigarg0000@gmail.com>
2021-08-01 09:59:10 +0530

    added view model class

---
+++ app/src/main/java/com/mani/wordwizard/WordViewModel.kt
@@ -0,0 +1,28 @@
+package com.mani.wordwizard
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+
+class WordViewModel(private val repository: WordRepository) : ViewModel() {
+    val getAllWords : LiveData<List<Word>> = repository.allWords.asLiveData()
+
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
+
+    fun onSwipe(word: Word) = viewModelScope.launch {
+        repository.delete(word)
+    }
+
+}
+
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+       if(modelClass.isAssignableFrom(WordViewModel::class.java)){
+           @Suppress("UNCHECKED_CAST")
+           return WordViewModel(repository) as T
+       }
+        throw IllegalArgumentException("unrecognized view model class")
+    }
+
+}
